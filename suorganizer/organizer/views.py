b5e331712583922c682a8acdd8d8dc5c4cd55736
musa <musa@Ronalds-MacBook-Air.local>
2017-02-26 22:03:08 -0600

    Wrote base template, organizer templates, post templates

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,9 +1,11 @@
+from django.template import Context, loader
 from django.http.response import HttpResponse
-
 from .models import Tag
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
+    template = loader.get_template('organizer/tag_list.html')
+    context = Context({'tag_list': tag_list})
+    output = template.render(context)
     return HttpResponse(output)
 # Create your views here.
