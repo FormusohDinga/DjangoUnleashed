171048476dc3a7d23fd16e7e70bba4ca958bc3ef
musa <musa@Ronalds-MacBook-Air.local>
2017-02-27 23:33:42 -0600

    changed urls.py to use TagCreate class

--- suorganizer/organizer/urls.py
+++ suorganizer/organizer/urls.py
@@ -1,20 +1,20 @@
 from django.conf.urls import url
 
-from .views import (tag_detail, tag_list, startup_list, startup_detail, tag_create)
+from .views import (tag_detail, tag_list, startup_list, startup_detail, TagCreate)
 
 urlpatterns = [
     url(r'^tag/$', tag_list,
-        name="organizer_tag_list" ),
+            name="organizer_tag_list" ),
     url(r'^tag/create/$',
-        tag_create,
-        name='organizer_tag_create'),
+            TagCreate.as_view(),
+            name='organizer_tag_create'),
     url(r'^tag/(?P<slug>[\w\-]+)/$',
-        tag_detail,
-        name="organizer_tag_detail"),
+            tag_detail,
+            name="organizer_tag_detail"),
     url(r'^startup/$',
-        startup_list,
-        name="organizer_startup_list"),
+            startup_list,
+            name="organizer_startup_list"),
     url(r'^startup/(?P<slug>[\w\-]+)/$',
-        startup_detail,
-        name="organizer_startup_detail"),
+            startup_detail,
+            name="organizer_startup_detail"),
 ]
