f117044ab3ac02b3cfaebd813e5f9bf6660f6c98
musa <musa@Ronalds-MacBook-Air.local>
2017-02-27 22:57:24 -0600

    created the tag_create() in organizer/views.py

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,9 +1,9 @@
 from django.http.response import HttpResponse
-from django.shortcuts import get_object_or_404
 from django.template import RequestContext, loader
-from django.shortcuts import render
+from django.shortcuts import (get_object_or_404 ,redirect,render)
 
 from .models import Tag, Startup
+from .forms import TagForm
 
 def tag_list(request):
     return render(request, 'organizer/tag_list.html',{'tag_list': Tag.objects.all()})
@@ -18,3 +18,16 @@ def startup_list(request):
 def startup_detail(request,slug):
     startup = get_object_or_404(Startup, slug__iexact=slug)
     return render(request, 'organizer/startup_detail.html', {'startup':startup})
+
+def tag_create(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            new_tag = form.save()
+            return redirect(new_tag)
+    else:  # request.method != 'POST'
+        form = TagForm()
+    return render(
+        request,
+        'organizer/tag_form.html',
+        {'form': form})
