d8e8dc7d6606ede845623234864e3a0a7a521e89
musa <musa@Ronalds-MacBook-Air.local>
2017-03-01 19:44:14 -0600

    completed through chapter 11

--- suorganizer/blog/urls.py
+++ suorganizer/blog/urls.py
@@ -1,16 +1,31 @@
 from django.conf.urls import url
-from .views import (PostCreate, PostList, post_detail)
+
+from .views import (
+    PostCreate, PostDelete, PostList, PostUpdate,
+    post_detail)
 
 urlpatterns = [
     url(r'^$',
-        PostList.as_view(template_name='blog/post_list.html'),
-        {'parent_template': 'base.html'},
+        PostList.as_view(),
         name='blog_post_list'),
-    url(r'^create/$', PostCreate.as_view(), name='blog_post_create'),
+    url(r'^create/$',
+        PostCreate.as_view(),
+        name='blog_post_create'),
     url(r'^(?P<year>\d{4})/'
         r'(?P<month>\d{1,2})/'
         r'(?P<slug>[\w\-]+)/$',
         post_detail,
-        {'parent_template': 'base.html'},
         name='blog_post_detail'),
+    url(r'^(?P<year>\d{4})/'
+        r'(?P<month>\d{1,2})/'
+        r'(?P<slug>[\w\-]+)/'
+        r'delete/$',
+        PostDelete.as_view(),
+        name='blog_post_delete'),
+    url(r'^(?P<year>\d{4})/'
+        r'(?P<month>\d{1,2})/'
+        r'(?P<slug>[\w\-]+)/'
+        r'update/$',
+        PostUpdate.as_view(),
+        name='blog_post_update'),
 ]
